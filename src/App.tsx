0eab514ec0132fe0fffd8585f21611997d5e7c1f
Athul krishna K S <athulkrishna736@gmail.com>
2025-04-08 11:45:26 +0530

    first commit second project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-red-500 text-white gap-4">
+      <h1 className="text-yellow-300 underline text-4xl">Tailwind Test 🔥</h1>
+      <p className="text-green-300 text-xl">Tailwind is working! 🚀</p>
+    </div>
+  );
+}
+
+export default App;
